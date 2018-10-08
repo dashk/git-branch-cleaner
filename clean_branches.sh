4c121d4f9b35f7c10a110c4aa5e7634c1a32641c
Kenny Wong <kwong@simondata.com>
2018-10-08 09:17:37 -0400

    1. Changed script to take in `master` branch name from prompt 2. Added checks to make sure user's in Git directory before    proceeding.

--- clean_branches.sh
+++ clean_branches.sh
@@ -2,13 +2,34 @@
 # @author dashk
 # @date 2016.06.22
 
-# Step 1: Navigate to master branch
-git checkout master
+# Step 1: Make sure you're in a git repo
+# Source: https://stackoverflow.com/questions/2180270/check-if-current-directory-is-a-git-repository
+is_inside_git_directory="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
 
-# Step 2: Get a list of branches that are NOT master
-branches=( $(git branch | grep -v 'master' | grep -v '\*' | awk '{print $1}') )
+if [ ! "$is_inside_git_directory" ]; then
+        echo "You must run this within a Git directory"
+        exit 1
+fi
 
-# Step 3: Determine if there are any branches we need to process
+# Step 2: Get name of master branch
+echo "What is the name of your master branch? (Press ENTER when done):"
+read masterBranchName
+echo "\r\n"
+
+if [ ! -z "$masterBranchName" -a "$masterBranchName" != " " ]; then
+        echo "Master branch name: $masterBranchName"
+else
+        echo "You must enter a master branch name."
+        exit 1
+fi
+
+# Step 3: Navigate to master branch
+git checkout $masterBranchName
+
+# Step 4: Get a list of branches that are NOT master
+branches=( $(git branch | grep -v $masterBranchName | grep -v '\*' | awk '{print $1}') )
+
+# Step 5: Determine if there are any branches we need to process
 branchCount=${#branches[@]}
 
 echo "Total # of non-master branches: ${branchCount}"
@@ -27,8 +48,9 @@ else
 
                 # Step 4.1 Delete branch per user request
                 if [[ $userInput == "Y" || $userInput == "y" ]]; then
-                        git branch -D $branch
-                        echo "Branch $branch deleted\r\n"
+                        echo "Pretending to delete $branch"
+                        #git branch -D $branch
+                        #echo "Branch $branch deleted\r\n"
                 fi
         done
 fi
